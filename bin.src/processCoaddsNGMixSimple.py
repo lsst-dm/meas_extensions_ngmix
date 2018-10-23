d45211481a67552fd0ab2a5e0ebff2c6eaba09c6
Erin Sheldon <erin.sheldon@gmail.com>
2018-10-23 14:45:49 -0500

    use NGMix instead of NgMix

--- bin.src/processCoaddsNGMixSimple.py
+++ bin.src/processCoaddsNGMixSimple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
 # fit a simple model
-from lsst.meas.extensions.ngmix import ProcessCoaddsNgMixSimpleTask
-ProcessCoaddsNgMixSimpleTask.parseAndRun()
+from lsst.meas.extensions.ngmix import ProcessCoaddsNGMixSimpleTask
+ProcessCoaddsNGMixSimpleTask.parseAndRun()
