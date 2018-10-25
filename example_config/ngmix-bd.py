f1a4a45d14eb7f3e17d8d77e79cd96bdc1e00e6d
Erin Sheldon <erin.sheldon@gmail.com>
2018-10-25 15:34:12 -0500

    added some features for bitmasks
    currently have a sub-config for dealing with
    getting an approximate background variance
    
    also added config for nulling parts of the weight
    map based on mask bits

--- example_config/ngmix-bd.py
+++ example_config/ngmix-bd.py
@@ -1,6 +1,6 @@
 # optionally limit the number of objects we process
 # the range is inclusive, not like a slice
-config.obj_range = [1000, 1009]
+#config.obj_range = [0, 199]
 
 #
 # postage stamp config
@@ -10,6 +10,68 @@ config.stamps.min_stamp_size = 32
 config.stamps.max_stamp_size = 256
 # how many "sigma" to make the radius of the box
 config.stamps.sigma_factor = 5.0
+
+# Jim comments "bright object" is just a geometric region around bright stars,
+# intended mostly for people doing things like cross-correlations that could be
+# impacted by e.g.  slight systematic effects in the background.  
+#
+# "clipped" means one or more input images was not used in this area (for large
+# regions that's usually because of a ghost, I think) - but the PSF model acts
+# as if that input image was used, so you'll probably see INEXACT_PSF set as
+# well, which indicates that the pixels and the PSF model assume different
+# things about the contributing images.
+#
+# I think I'd say for all of those that we should definitely proceed with
+# photometric fitting, but whether we can do shear in those areas is
+# questionable for bright star (I think the regions are probably conservatively
+# large right now), and we definitely can't do shear in INEXACT_PSF areas.
+
+# this is for our kludge to get the background noise
+# by taking the median of the pixels without these bits
+# set.  we should probably exclude the pixels that are part
+# of the objects as well
+config.stamps.bits_to_ignore_for_weight = [
+    # for coadd assume OK since will be interpolated on one of the epochs
+    'BAD',
+    'SAT',
+    'INTRP',
+    'CR',
+    'EDGE',
+    'SUSPECT',
+    'NO_DATA',
+    #'BRIGHT_OBJECT',
+    # only appears in coadd, may want to cut on this for shear
+    #'CLIPPED',
+    'CROSSTALK',
+    # only matter if we are using the deblended photometry, which we are not here
+    #'NOT_DEBLENDED',
+    'UNMASKEDNAN',
+]
+
+
+# mask bits to null in the weight map. We need to think what we will do
+# for metacal here
+config.stamps.bits_to_null = [
+    # for coadd assume OK since will be interpolated on one of the epochs
+    #'BAD',
+    'SAT',
+    # Jim says "cut on the reason it was interpolated rather than this"
+    #'INTRP',
+    # for coadd assume OK since will be interpolated on one of the epochs
+    #'CR',
+    'EDGE',
+    'SUSPECT',
+    'NO_DATA',
+    # Jim suggests we cut these after the fact in the bright star mask
+    #'BRIGHT_OBJECT',
+    # only appears in coadd, may want to cut on this for shear
+    #'CLIPPED',
+    #'CROSSTALK',
+    # only matter if we are using the deblended photometry, which we are not here
+    #'NOT_DEBLENDED',
+    'UNMASKEDNAN',
+]
+
 
 ###############
 # object config
