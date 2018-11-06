3c4dc705505928b21b113335206833c24ab807e7
Erin Sheldon <erin.sheldon@gmail.com>
2018-11-06 13:38:40 -0600

    add IMAGE_FLAGS bit

--- python/lsst/meas/extensions/ngmix/procflags.py
+++ python/lsst/meas/extensions/ngmix/procflags.py
@@ -5,7 +5,7 @@ GAL_FIT_FAILURE      = 2**2
 
 # flags used by NGMixer
 #BAD_OBJ              = 2**25
-#IMAGE_FLAGS          = 2**26
+IMAGE_FLAGS          = 2**26
 #NO_CUTOUTS           = 2**27
 #BOX_SIZE_TOO_BIG     = 2**28
 #UTTER_FAILURE        = 2**29
